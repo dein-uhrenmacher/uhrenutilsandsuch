c6799618c8b05a1185b7cd7b1dde861ac2a69390
dein-uhrenmacher <70207003+dein-uhrenmacher@users.noreply.github.com>
2020-09-07 19:47:52 +0200

    Add files via upload

--- uhrenutils.py
+++ uhrenutils.py
@@ -4,7 +4,22 @@ import socket
 import sys
 
 def help():
-   dsfsdf
+    print('Read the Readme....god damn')
 if sys.argv[1].startswith("-help"):
-    print(1)
+    help()
+
+def portscan():
+    target = "uhrenmacher.onyx-7.de"
+    targetIP = socket.gethostbyname(target)
+    try:
+        for p in range(1, 30):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            res = sock.connect_ex((targetIP, p))
+            if res == 0:
+                print("[*] Open port at  " + str(p))
+            sock.close()
+    except Exception:
+        print("[*] Error")
+        sys.exit()
+    print("Scan completed in")
 
